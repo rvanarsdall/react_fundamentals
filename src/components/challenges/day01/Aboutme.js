a599f36cfd0de50c1796bc3306f36819d74bd97d
Paul Niemczyk <mrpndev@gmail.com>
2021-01-26 21:21:06 -0500

    Created functional components

---
+++ src/components/challenges/day01/Aboutme.js
@@ -0,0 +1,16 @@
+const Aboutme = () => {
+    return (
+        <div>
+            <h1>Paul</h1>
+            <p>I need sleep</p>
+            <ul>
+                <li>True</li>
+                <li>Lie</li>
+                <li>True</li>
+            </ul>
+        </div>
+
+    )
+};
+
+export default Aboutme

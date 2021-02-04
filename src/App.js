24b62b8eb4de7ce441513464f23b6d796278681d
Robert Vanarsdall <RV18391@homedefenders.com>
2021-02-04 17:59:56 -0500

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import Header from './components/Header'
-import Aboutme from './components/challenges/day01/Aboutme'
-import Home from './components/Home'
-import Footer from './components/Footer'
+import "./App.css";
+import Header from "./components/Header";
+import Aboutme from "./components/challenges/day01/Aboutme";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
-function App() { // this is a root component
-                // parent function
-  const name = 'Paul' // JS above return statement can be
-                      // injected into JSX using { }
+function App() {
+  // this is a root component
+  // parent function
+  const name = "Paul"; // JS above return statement can be
+  // injected into JSX using { }
 
   return (
     <div className="App">
-      <Header /> { /* this is how you mount a component */ }
-      <Aboutme /> { /* child function */ }
+      <Header /> {/* this is how you mount a component */}
+      <Aboutme /> {/* child function */}
       <Home />
       <Footer />
     </div>
